0600ee2228b86b6547a56a25f0e0b4f112c5771c
Hugo Cauzid <hcauzid@hotmail.fr>
2026-02-18 11:57:09 +0100

    E2 : Lien yutube fonctionnel, audio et vidéo internes non fonctionnels

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* <iframe
+        width={1000}
+        src="//www.youtube.com/embed/tJjxKhjR9H4"
+      ></iframe> */}
+
+      <video width={1000} controls>
+        <source src={"./Endless_Possibility.mp4"} type="video/mp4" />
+        <source src={"./Endless_Possibility.ogg"} type="video/ogg" />
+      </video>
+
+      <audio controls>
+        <source src={"./Endless_Possibility.ogg"} type="audio/ogg"></source>
+        <source src={"./Endless_Possibility.mp3"} type="audio/mpeg"></source>
+        // possible que le navigateur n'affiche pas
+      </audio>
     </View>
   );
 }
@@ -13,8 +28,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+{
+  /* <iframe width={420} height={315} src="https://www.youtube.com/watch?v=tJjxKhjR9H4"></iframe>; */
+} // Vidéo à partir d'un lien youtube à intégrer
+
+{
+  /*
+  <video width={420} height={315}>
+  <source src={fileMP4} type="video/mp4"></source>
+  <source src={fileOGG} type="video/ogg"></source>
+  // possible que le navigateur n'affiche pas
+</video>;*/
+} // Vidéo à partir d'un fichier mp4
+
+{
+  /*
+  <audio controls> // controls donne accès aux contrôles à l'utilisateur
+  <source src={fileMP4} type="video/mp4"></source>
+  <source src={fileOGG} type="video/ogg"></source>
+  // possible que le navigateur n'affiche pas
+</audio>;*/
+} // Audio à partir d'un fichier mp4
